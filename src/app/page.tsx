0395edae9e1b0642e10f044ab2d45433a9dcc02f
Witold <witekrychlik@o2.pl>
2024-02-10 15:37:44 +0100

    feat: rest api tested

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,11 @@
-export default function Home() {
+const getData = async () => {
+  const res = await fetch(`${process.env.NEXT_PUBLIC_SERVER_URL}/api/posts`);
+  return res.json();
+};
+
+export default async function Home() {
+  const data = await getData();
+  console.log(data);
   return (
     <main>
       <div>Hello Marian!</div>
